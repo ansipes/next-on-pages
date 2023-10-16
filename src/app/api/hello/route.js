a8d9d100988c82d1952626c29824cbd7f27aec7e
Andrew Sipes <andrew.n.sipes@gmail.com>
2023-10-16 11:37:50 -0400

    fix: api endpoint

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,8 +1,19 @@
-
 // Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
-export async function GET(request) {
-  return new Response(JSON.stringify({ name: 'John Doe' }))
+// export async function GET(request) {
+//   return new Response(JSON.stringify({ name: "Andrew Sipes" }));
+// }
+
+export default async function GET(request) {
+  try {
+    const response = await fetch(
+      "https://api.dictionaryapi.dev/api/v2/entries/en/keyboard"
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching data" });
+  }
 }
